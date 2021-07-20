7be007ded84ae3d630c404524cfc0b627ded3c67
styluo <shu_jiahuili@foxmail.com>
2021-07-20 11:59:21 +0800

    feat: 支持物品模型

--- Demo.php
+++ Demo.php
@@ -1,29 +1,39 @@
 <?php
 
-include_once "GrowingIO.php";
+use com\growingio\GrowingIO;
+
+include_once 'GrowingIO.php';
 
 // 请在您调试前，将accountID修改为您的项目AccountID
 // 所有自定义事件需要提前在GrowingIO产品中进行定义
 // 所有自定义事件的属性也需要提前在GrowingIO产品中进行定义
 
-$accountID = "YOUR_ACCOUNT_ID"; // 项目 ID，见数据源配置
-$host = "http://YOUR_COLLECTOR_DOMAIN"; // 数据收集服务域名，请参考运维手册或联系技术支持获取
-$dataSourceId = "YOUR_DATASOURCE_ID"; // 数据源 ID，见数据源配置
-$props = array("debug"=>false); // debug 模式，此模式仅打印日志，不发送数据
+$accountID = '1234567887654321'; // 项目 ID，见数据源配置
+$host = 'https://localhost.com'; // 数据收集服务域名，请参考运维手册或联系技术支持获取
+$dataSourceId = '12345678'; // 数据源 ID，见数据源配置
+$props = array('debug' => true); // debug 模式，此模式仅打印日志，不发送数据
 
 $gio = GrowingIO::getInstance($accountID, $host, $dataSourceId, $props);
 
-function currentMillisecond() {
+function currentMillisecond()
+{
     list($msec, $sec) = explode(' ', microtime());
     $msectime = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
     return $msectime;
 }
 
 $start = currentMillisecond();
-printf($start."\n");
-$gio->track("testUserId","testEvent", array("eventKey2"=>"v1", "eventKey2"=>"v2"));
-$gio->setUserAttributes("testUserId", array("userKey1"=>"v1", "userKey2"=>"v2"));
+printf($start . PHP_EOL);
+$gio->track('phpUserId', 'phpEvent');
+$gio->setUserAttributes('phpUserId', array('userKey1' => 'v1', 'userKey2' => 'v2'));
+$gio->track(
+    'phpUserId',
+    'phpEvent',
+    array('userKey1' => 'v1', 'userKey2' => 'v2'),
+    '1',
+    'phpKey'
+);
+$gio->setItemAttributes('1', 'phpKey', array('phpAttrKey' => 'phpAttrValue', 'phpColor' => 'red'));
 $stop = currentMillisecond();
-printf($stop."\n");
-printf(($stop-$start)."\n");
-?>
+printf($stop . PHP_EOL);
+printf(($stop - $start) . PHP_EOL);
